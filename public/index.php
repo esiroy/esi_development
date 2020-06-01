115d723aa068ae57facc24a751568633119d419b
esiroy <esi.roy.dev@gmail.com>
2020-06-01 12:24:40 -0600

    updated for blue host server

--- public/index.php
+++ public/index.php
@@ -21,7 +21,11 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
+//require __DIR__.'/../vendor/autoload.php';
+
+
 require __DIR__.'/../vendor/autoload.php';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +39,11 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
+//$app = require_once __DIR__.'/../bootstrap/app.php';
+
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
 
 /*
 |--------------------------------------------------------------------------
