7ec8c0782bfa3a784e7561572b010db8f071122c
unknown <esi.roy.dev@gmail>
2021-01-09 14:06:51 +0800

    login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use App\Models\User;
+use Hash, Auth, Str;
+
 
 class LoginController extends Controller
 {
@@ -37,4 +42,55 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Overwrite default login method to in order to allow user to use old MD5 Hash passwords
+     * and migrate it without asking him any change
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        // check against old md5 password, if correct, create bcrypted updated pswd
+        $user = User::where('username', $request->username)->first();
+        
+
+        if( $user && $user->password == md5($request->password))
+        {
+            $user->password = Hash::make($request->password); //update the password to better hasher accordingly
+            $user->save();
+        }
+
+        /*
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }*/
+
+        /** @description - only valid user can login */
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt(['username' => $request->username, 'password' => $request->password, 'valid' => 1])) {
+            // Authentication passed...
+            $user->api_token = Hash('sha256', Str::random(80)); //update api token for old md5 passowrd since older user is having md5 encryption
+            $user->save();
+            return redirect()->intended('home');
+        }
+        
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
 }
