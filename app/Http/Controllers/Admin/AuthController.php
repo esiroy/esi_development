c86dd82dd1000839378f1a0b1d690bd6379618ed
unknown <esi.roy.dev@gmail>
2021-01-09 14:38:54 +0800

    sidebar reports

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -51,12 +51,15 @@ class AuthController extends Controller
 
         // check against old md5 password, if correct, create bcrypted updated pswd
         $user = User::where('username', $request->username)->first();
-        
 
-        if( $user && $user->password == md5($request->password))
+        if (strlen($request->password) == 32 && ctype_xdigit($request->password)) 
         {
-            $user->password = Hash::make($request->password); //update the password to better hasher accordingly
-            $user->save();
+            if( $user && $user->password == md5($request->password))
+            {
+                $user->password = Hash::make($request->password); //update the password to better hasher accordingly
+                $user->save();
+            }
+    
         }
 
         /*
