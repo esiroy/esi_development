ec19a9ce6093b0724df87340c115e52cbc552149
unknown <esi.roy.dev@gmail>
2021-01-09 12:20:47 +0800

    compare table importer

--- app/Http/Controllers/TableImporterController.php
+++ app/Http/Controllers/TableImporterController.php
@@ -43,13 +43,34 @@ class TableImporterController extends Controller
         foreach ($localItems as $item) {
             $itemLocalArray[$item->id] = $item->id;
         }
-
-
-
-        echo "<pre>";
+        
         $result = array_diff($itemLiveArray, $itemLocalArray);
 
-        print_r ($result);
+        foreach ($results as $item) 
+        {
+            $items = DB::connection('mysql_live')->select("select * from agent_transaction where member_id = $memberID");
+
+            $data = [
+                'id' => $item->id,
+                'created_at' => $item->created_on,
+                'updated_at' => $item->updated_on,
+                'valid' => $item->valid,
+                'amount' => $item->amount,
+                'remarks' => $item->remarks,
+                'transaction_type' => $item->transaction_type,
+                'agent_id' => $item->agent_id,
+                'created_by_id' => $item->created_by_id,
+                'member_id' => $item->member_id,
+                'schedule_item_id' => $item->schedule_item_id,
+                'price' => $item->price,
+                'lesson_shift_id' => $item->lesson_shift_id,
+                'credits_expiration' => $item->credits_expiration,
+                'old_credits_expiration' => $item->old_credits_expiration,
+            ];
+
+            $transaction = AgentTransaction::insert($data);  
+            echo "<div style='color:blue'>$ctr - Added : " . $item->id . " " . $item->created_on . "</div>";    
+        }
 
     }
 
