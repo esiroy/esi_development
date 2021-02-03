3b9449ee87ce9275d76d1f6d846482ee8fc6cf5c
unknown <esi.roy.dev@gmail>
2021-02-03 17:48:54 +0800

    report update

--- app/Http/Controllers/Admin/Modules/ReportController.php
+++ app/Http/Controllers/Admin/Modules/ReportController.php
@@ -55,7 +55,7 @@ class ReportController extends Controller
         }
 
        
-        return view('admin.modules.report.index', compact('schedules', 'date', 'from', 'to'));
+        return view('admin.modules.report.index', compact('schedules', 'from', 'to'));
     }
 
 
