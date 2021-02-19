031c6a033274ecbdae43a2304003e173ace7f90e
unknown <esi.roy.dev@gmail>
2021-02-19 22:04:13 +0800

    lesson material text for informating client

--- resources/views/modules/lessonmaterials/materials.blade.php
+++ resources/views/modules/lessonmaterials/materials.blade.php
@@ -34,7 +34,7 @@
                     <div class="card esi-card">
                         <div class="card-header esi-card-header">
                             レッスン教材 
-                            <span class="text-right" style="width: 70%;display: block; float: right;font-size: 15px; color: #000; margin:4px 0px 0px">
+                            <span class="text-right" style="width: 75%;display: block; float: right;font-size: 15px; color: #000; margin:4px 0px 0px">
                                 (オリジナル教材 以外 は講師またはカスタマーサ ポート までお尋ねください ）
                             </span>
                         </div>
