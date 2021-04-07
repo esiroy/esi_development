0d37a6654d654cb5b584f559478252f707cbed33
Roy Robert Abellana <abellana@gmail.com>
2021-04-08 01:18:44 +0800

    update export to read expired

--- app/Http/Controllers/dummyController.php
+++ app/Http/Controllers/dummyController.php
@@ -86,7 +86,8 @@ class dummyController extends Controller
         $memberQuery = Member::join('agent_transaction', 'agent_transaction.member_id', '=', 'members.user_id');
         $memberQuery = $memberQuery->whereBetween('agent_transaction.created_at', array($dateFrom, $dateTo));
         $memberQuery = $memberQuery->where('agent_transaction.transaction_type', "LIKE", "EXPIRED");
-        $memberQuery = $memberQuery->whereDate('members.credits_expiration', '<', $dateTo);  //expired
+
+        $memberQuery = $memberQuery->where('members.credits_expiration', "");  //expired
 
         $memberQuery = $memberQuery->get();        
 
