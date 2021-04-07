68d571775328fee6583b0f591ff5390e8f4cb77b
Roy Robert Abellana <abellana@gmail.com>
2021-04-08 04:06:38 +0800

    add agent transaction

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -67,8 +67,8 @@ class ExportController extends Controller
         //get query with expiration null
         $memberQuery = Member::join('agent_transaction', 'agent_transaction.member_id', '=', 'members.user_id');
         $memberQuery = $memberQuery->whereBetween('agent_transaction.created_at', array($dateFrom, $dateTo));
-        //$memberQuery = $memberQuery->where('agent_transaction.transaction_type', "LIKE", "EXPIRED");
-        //$memberQuery = $memberQuery->where('members.membership', "Point Balance");
+        $memberQuery = $memberQuery->where('agent_transaction.transaction_type', "LIKE", "EXPIRED");
+        $memberQuery = $memberQuery->where('members.membership', "Point Balance");
         $memberQuery = $memberQuery->where('members.credits_expiration', null);  //expired
         $memberQuery = $memberQuery->groupby('members.user_id')->get()->toArray();
         
@@ -79,6 +79,7 @@ class ExportController extends Controller
         $memberQueryOne = $memberQueryOne->whereDate('members.credits_expiration', '<=', $dateTo);
         $memberQueryOne = $memberQueryOne->where('membership', "Point Balance");        
         $memberQueryOne = $memberQueryOne->orderby('members.credits_expiration', 'ASC')->get()->toArray();
+
         $memberQuery = array_merge($memberQuery, $memberQueryOne);
 
         //get query with expiration null
@@ -86,7 +87,9 @@ class ExportController extends Controller
         $memberQueryThree = $memberQueryThree->whereBetween('members.credits_expiration', array($dateFrom, $dateTo));
         $memberQueryThree = $memberQueryThree->where('members.membership', "Point Balance");
         $memberQueryThree = $memberQueryThree->groupby('members.user_id')->get()->toArray();
+
         $memberQueryAll = array_merge($memberQuery, $memberQueryThree);
+
         $memberQueryAll = unique_multidim_array($memberQueryAll, 'user_id');
 
         //Agent Credits Initialize
