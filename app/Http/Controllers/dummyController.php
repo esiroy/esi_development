70ba22a599a3d3ea92adaa6b5fb35857f5804ebf
Roy Robert Abellana <abellana@gmail.com>
2021-04-08 01:55:43 +0800

    add agent expired

--- app/Http/Controllers/dummyController.php
+++ app/Http/Controllers/dummyController.php
@@ -86,7 +86,7 @@ class dummyController extends Controller
         $memberQuery = Member::join('agent_transaction', 'agent_transaction.member_id', '=', 'members.user_id');
         $memberQuery = $memberQuery->whereBetween('agent_transaction.created_at', array($dateFrom, $dateTo));
         $memberQuery = $memberQuery->where('agent_transaction.transaction_type', "LIKE", "EXPIRED");
-        $memberQuery = $memberQuery->where('members.membership', "Point Balance");
+        //$memberQuery = $memberQuery->where('members.membership', "Point Balance");
         $memberQuery = $memberQuery->where('members.credits_expiration', null);  //expired
         $memberQuery = $memberQuery->groupby('members.user_id')->get()->toArray();
 
@@ -98,7 +98,7 @@ class dummyController extends Controller
         $memberQueryOne = $memberQueryOne->where('members.credits_expiration', ">=", $dateFrom);
         $memberQueryOne = $memberQueryOne->whereDate('members.credits_expiration', '<=', $dateTo);
         $memberQueryOne = $memberQueryOne->where('membership', "Point Balance");
-        $memberQueryOne = $memberQueryOne->where('members.credits_expiration', null);  //expired
+        //$memberQueryOne = $memberQueryOne->where('members.credits_expiration', null);  //expired
         $memberQueryOne = $memberQueryOne->orderby('members.credits_expiration', 'ASC')->get()->toArray();
 
         $memberQueryAll = array_merge($memberQuery, $memberQueryOne);
