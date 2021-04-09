812ef487660b35c856ea0335d23c54c2ad8f9ccc
Roy Robert Abellana <abellana@gmail.com>
2021-04-09 20:04:18 +0800

    mailer formatter for cust sup

--- resources/views/emails/customersupport_to_admin_plain.blade.php
+++ resources/views/emails/customersupport_to_admin_plain.blade.php
@@ -1,21 +1,15 @@
 
 {{ date('M d, Y')}} 
-{{PHP_EOL}} {{PHP_EOL}} 
-
+{{PHP_EOL}} 
 
 お名前 （必須）: {{ $member->user->firstname }}  {{ $member->user->lastname }}
-{{PHP_EOL}} {{PHP_EOL}} 
-
+{{PHP_EOL}}
 
 フリガナ: {{ $member['nickname'] }}
-{{PHP_EOL}}{{PHP_EOL}} 
-
+{{PHP_EOL}}
  
 ご登録メールアドレス （必須）: {{ $member->user->email }} 
-{{PHP_EOL}} {{PHP_EOL}} 
-
+{{PHP_EOL}}
 
 お問い合わせ内容 （必須）: {{ $data['inquiry']}} 
-{{PHP_EOL}} {{PHP_EOL}} 
-
-
+{{PHP_EOL}}
