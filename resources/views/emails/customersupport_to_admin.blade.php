06cb3fe89f06b9ff1bf8167dc6d5e9e4a20985e6
Roy Robert Abellana <abellana@gmail.com>
2021-04-09 21:50:50 +0800

    customer template email update

--- resources/views/emails/customersupport_to_admin.blade.php
+++ resources/views/emails/customersupport_to_admin.blade.php
@@ -1,5 +1,6 @@
-{{ date('M d, Y')}} <br/><br/>
-お名前 （必須）: {{ $member->user->firstname }}  {{ $member->user->lastname }}<br/><br/>
-フリガナ: {{ $member['nickname'] }}<br/><br/> 
-ご登録メールアドレス （必須）: {{ $member->user->email }} <br/><br/>
-お問い合わせ内容 （必須）: {{ $data['inquiry']}} <br/><br/>
+{{ date('M d, Y')}} <br/>
+お名前 （必須）: {{ $member->user->firstname }}  {{ $member->user->lastname }}<br/>
+Name: {{ $member->user->lastname ?? ''}}, {{ $member->user->firstname ?? '' }}<br/>
+フリガナ: {{ $member['nickname'] }}<br/> 
+ご登録メールアドレス （必須）: {{ $member->user->email }} <br/>
+お問い合わせ内容 （必須）: {{ $data['inquiry']}} <br/>
