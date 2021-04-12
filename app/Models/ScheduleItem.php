47d479d9e0cc774d2035ad7054686f7dcdcd66c6
Roy Robert Abellana <abellana@gmail.com>
2021-04-12 22:29:33 +0800

    update counter for schedule

--- app/Models/ScheduleItem.php
+++ app/Models/ScheduleItem.php
@@ -66,7 +66,8 @@ class ScheduleItem extends Model
                     ->whereYear('lesson_time', '=', $currentYear)
                     ->whereMonth('lesson_time','=', $currentMonth)
                     ->where('schedule_status', '=', "CLIENT_RESERVED")                       
-                    ->where('valid', 1)->count();
+                    //->where('valid', 1)
+                    ->count();
 
       
 
@@ -75,19 +76,22 @@ class ScheduleItem extends Model
                     ->whereYear('lesson_time', '=', $currentYear)
                     ->whereMonth('lesson_time','=', $currentMonth)
                     ->where('schedule_status', '=', "CLIENT_RESERVED_B")                       
-                    ->where('valid', 1)->count();                    
+                    //->where('valid', 1)
+                    ->count();                    
                     
         $completed = ScheduleItem::where('member_id', $memberID)
                     ->whereYear('lesson_time', '=', $currentYear)
                     ->whereMonth('lesson_time','=', $currentMonth)
                     ->where('schedule_status', '=', "COMPLETED")                       
-                    ->where('valid', 1)->count();
+                    //->where('valid', 1)
+                    ->count();
 
         $not_available = ScheduleItem::where('member_id', $memberID)
                         ->whereYear('lesson_time', '=', $currentYear)
                         ->whereMonth('lesson_time','=', $currentMonth)
                         ->where('schedule_status', '=', "CLIENT_NOT_AVAILABLE")                       
-                        ->where('valid', 1)->count();
+                        //->where('valid', 1)
+                        ->count();
                         
         $reserveCount = $reserved + $reserved_b + $completed + $not_available;
 
