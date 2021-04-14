aa46c414257f698144e9c63829252392ddf78e6a
Roy Robert Abellana <abellana@gmail.com>
2021-04-14 21:29:37 +0800

    test

--- resources/views/admin/test/index.blade.php
+++ resources/views/admin/test/index.blade.php
@@ -5,9 +5,38 @@
     <div class="row">
         <div class="col-md-12">
             <input type="date" class="inputDate form-control form-control-sm col-sm-12 col-md-12">
+
+
+            <div id="input_container">
+                <input type="text" id="input" value>
+                <img src="https://cdn1.iconfinder.com/data/icons/CornerStone/PNG/arrow%20right.png" id="input_img">
+            </div>
+
+
+            <div class="form-group has-feedback">
+                <label class="control-label">Username</label>
+                <input type="date" class="inputDate form-control form-control-sm col-sm-12 col-md-12" placeholder="Username" />
+               <i class="far fa-calendar"></i>
+            </div>
+
+
         </div>
     </div>
 </div>
+@endsection
+
+@section('styles')
+@parent
+<style>
+
+.inputDate::after {    
+    font-family: "Font Awesome\ 5 Free";
+    content: "\F133";
+    padding-right:200px;
+    padding-left: 200px;
+}
+
+</style>
 @endsection
 
 @section('scripts')
