8da84fb42ef216aec688ac1cc2a3020a3d2d395f
Roy Robert Abellana <abellana@gmail.com>
2021-04-16 19:04:58 +0800

    test

--- app/Http/Controllers/Admin/Modules/QuestionnaireController.php
+++ app/Http/Controllers/Admin/Modules/QuestionnaireController.php
@@ -74,7 +74,7 @@ class QuestionnaireController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request)
     {
 
         $scheduleItem = ScheduleItem::find($id);
