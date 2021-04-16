a96f9b20fcaeae7afb7df213b734e2926597d24d
Roy Robert Abellana <abellana@gmail.com>
2021-04-16 19:09:00 +0800

    test

--- app/Http/Controllers/Admin/Modules/QuestionnaireController.php
+++ app/Http/Controllers/Admin/Modules/QuestionnaireController.php
@@ -79,6 +79,11 @@ class QuestionnaireController extends Controller
 
         $scheduleItem = ScheduleItem::find($id);
 
+        echo $scheduleItem->tutor_id;
+
+        exit();
+        
+
         $member = Member::where('user_id', $scheduleItem->member_id)->first();
         $userImage = UserImage::where('user_id', $member->user_id)->first();
         
