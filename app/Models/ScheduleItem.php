75aea20ab834f9b98f231cef06bc680bcd868525
abellan, roy robert <abellana@gmail.com>
2021-05-11 18:54:16 +0800

    update controller for api, hide false schedule or inactive

--- app/Models/ScheduleItem.php
+++ app/Models/ScheduleItem.php
@@ -353,8 +353,8 @@ class ScheduleItem extends Model
                 $hasReportCard = false;
             }
 
-            //if ($item->valid === 1 || $item->valid === '1') 
-            //{                
+            if ($item->valid === 1 || $item->valid === '1') 
+            {                
                 $schedules[$item->tutor_id][date('Y-m-d', strtotime($item->lesson_time))][date("H:i", strtotime($item->lesson_time . " -1 hour"))] = [
                     'valid' => $item->valid,
                     'id' => $item->id,
@@ -370,7 +370,7 @@ class ScheduleItem extends Model
                     'hasQuestionnaire' => $hasQuestionnaire
                     //'questionnaire' => $questionnaire,
                 ];
-            //}
+            }
 
 
         }
