068d9b777a69f3967ce1c31c5c9b0f67276ee8f9
abellan, roy robert <abellana@gmail.com>
2021-05-13 00:10:50 +0800

    test questionnaire

--- resources/views/admin/modules/questionnaires/test_show.blade.php
+++ resources/views/admin/modules/questionnaires/test_show.blade.php
@@ -2,10 +2,7 @@
 
 @section('content')
 
-    @if ($userImage == null)
-        <img src="{{ Storage::url('user_images/noimage.jpg') }}" class="img-fluid border" alt="no photo uploaded">
-    @else
-        <img src="{{ Storage::url("$userImage->original") }}" class="img-fluid border" alt="profile photo">
-    @endif
-
+  
+  test_show!!!
+  
 @endsection
