bc6c8d524fc2850c957167f6e6431f6f9896c58f
abellan, roy robert <abellana@gmail.com>
2021-05-13 00:24:20 +0800

    test questionnaire

--- resources/views/admin/modules/questionnaires/test_show.blade.php
+++ resources/views/admin/modules/questionnaires/test_show.blade.php
@@ -3,6 +3,15 @@
 @section('content')
 
   
+
+    @if ($userImage == null)
+        <img src="{{ Storage::url('user_images/noimage.jpg') }}" class="img-fluid border" alt="no photo uploaded">
+    @else
+        <img src="{{ Storage::url("$userImage->original") }}" class="img-fluid border" alt="profile photo">
+    @endif
+
+
+
     test grade 1: {{  $questionnaireItem1->grade ?? "" }}
 
     test grade 2: {{  $questionnaireItem1->grade ?? "" }}
