1d384ada1c3e1ae7602ed3ec889f519cae290651
roy <abellana@gmail.com>
2021-05-31 14:47:46 +0800

    update the google tag

--- resources/views/auth/activation.blade.php
+++ resources/views/auth/activation.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.esi-public')
+@extends('layouts.esi-public-activation')
 
 @section('content')
 <div class="container">
---
+++ resources/views/layouts/esi-public-activation.blade.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="My Tutor">
+    <meta name="keywords" content="Tutor, Japan, Lesson">
+    <title>{{ config('app.name', 'My Tutor') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+    <link rel="preconnect" href="//fonts.gstatic.com"  crossorigin />
+    <link rel="preconnect" href="//fonts.googleapis.com"  crossorigin />
+
+    <!-- Styles -->
+    <link rel="icon" href="{{ url('images/favicon.ico') }}"/>        
+    <link rel="preload" href="{{ asset('css/app.css') }}" as="style">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
+    <noscript>
+        <link rel="stylesheet" type="text/css" href="">
+    </noscript>
+</head>
+<body>
+
+    <img src="https://advack.net/m/img.php?pcode=248&aid=14205" width="1" height="1" />   
+
+    <!-- Google Code for &#20250;&#21729;&#30331;&#37682; Conversion Page -->
+    <script type="text/javascript">
+        / <![CDATA[ /
+        var google_conversion_id = 1013785582;
+        var google_conversion_language = "en";
+        var google_conversion_format = "2";
+        var google_conversion_color = "ffffff";
+        var google_conversion_label = "-1x3CLKOuwUQ7se04wM";
+        var google_conversion_value = 1.00;
+        var google_conversion_currency = "JPY";
+        var google_remarketing_only = false;
+        / ]]> /
+
+        /*
+        a8sales({
+        "pid": "s00000000062001",
+        "order_number": "12834",
+        "currency": "JPY",
+        "items": [
+        {
+        "code": "a8",
+        "price": 926,
+        "quantity": 1
+        },
+        ],
+        "total_price": 926,
+        }); 
+        */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+
+    <script src="//statics.a8.net/a8sales/a8sales.js"></script>
+    <script>
+    a8sales({
+        "pid": "s00000000062001",
+        "order_number": "14205",
+        "currency": "JPY",
+        "items": [
+            {
+            "code": "a8",
+            "price": 926,
+            "quantity": 1
+            },
+        ],
+        "total_price": 926,
+    });
+    </script>
+		
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt=""  src="//www.googleadservices.com/pagead/conversion/1013785582/?value=1.00&amp;currency_code=JPY&amp;label=-1x3CLKOuwUQ7se04wM&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript> 
+
+
+    <div id="app">
+        
+        <!--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm"  style="background-color:#2d9bb6">-->
+
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm"  style="background-color:#2d9bb6">
+            <div class="container">
+
+            <!--
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'My Tutor') }}
+                </a>
+
+            -->
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link text-white font-weight-bold" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+
+                            <!--
+                            @if (Route::has('signup'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('signup') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                            -->
+                            
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ ucfirst(Auth::user()->first_name) }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout_member') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="main-container py-4">
+            @yield('content')
+        </main>
+
+        <!--
+        <footer class="pt-4 pt-md-4 pb-md-4 border-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md text-center">
+                        <a class="navbar-brand" href="{{ url( route('home') ) }}">
+                            {{ config('app.name', 'My Tutor') }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        -->
+
+    </div>
+
+    @yield('scripts')
+</body>
+</html>
