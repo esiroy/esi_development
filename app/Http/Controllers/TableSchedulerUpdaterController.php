a71940da980fca94b230be4fa0eb8d02bb95210f
roy <abellana@gmail.com>
2021-06-02 15:51:55 +0800

    updater for schedules

--- app/Http/Controllers/TableSchedulerUpdaterController.php
+++ app/Http/Controllers/TableSchedulerUpdaterController.php
@@ -19,10 +19,49 @@ class TableSchedulerUpdaterController extends Controller
         ->whereDate('lesson_time', $dateFrom )
         ->get();
 
+        $ctr = 0;
 
         foreach ($items as $item) {
             $url = url("scheduleUpdater/$item->id");
-            echo "<a href='$url'> $item->id - $item->lesson_time </a><br>";
+            //echo "<a href='$url'> $item->id - $item->lesson_time </a><br>";
+
+            $data = [
+                'id' => $item->id,
+                'created_at' => $item->created_on,
+                'updated_at' => $item->updated_on,
+                'valid' => $item->valid,
+                'lesson_time' => $item->lesson_time,
+                'tutor_id' => $item->tutor_id,
+                'member_id' => $item->member_id,
+                'schedule_status' => $item->schedule_status,
+                'duration' => $item->duration,
+                'lesson_shift_id' => $item->lesson_shift_id,
+                'memo' => $item->memo,                    
+                //'email_type' => $liveItem->email_type,                 
+            ];
+            
+    
+            if (ScheduleItem::where('id', $item->id)->exists()) 
+            {
+                $ctr = $ctr + 1;
+
+                $scheduleItem = ScheduleItem::where('id', $item->id)->first();
+                $transaction = $scheduleItem->update($data);
+                
+                echo "<pre>";
+                print_r ($data);
+                echo "</pre>";
+                
+                echo "<div style='color:green'>$ctr - updated : " . $item->id . " " . $item->lesson_time . "</div>";
+            } else {
+
+                $ctr = $ctr + 1;
+
+                $transaction = ScheduleItem::insert($data);
+                echo "<div style='color:blue'>$ctr - Added : " . $item->id . " " . $item->lesson_time . "</div>";
+            }            
+
+
         }
 
     }
