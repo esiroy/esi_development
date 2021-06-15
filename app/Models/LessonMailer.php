77147285da990bd7714aaa45a478b053089b10b3
roy <abellana@gmail.com>
2021-06-15 18:43:58 +0800

    client reserved b

--- app/Models/LessonMailer.php
+++ app/Models/LessonMailer.php
@@ -85,7 +85,7 @@ class LessonMailer extends Model
 
         } else if ($selectedSchedule->schedule_status == 'CLIENT_RESERVED_B') {
 
-            $memberEmailData['template'] = "emails.manager.clientReserved_b";
+            $memberEmailData['template'] = "emails.client.reserved";
             $memberEmailData['subject'] = 'マイチューター：レッスン予約のご案内'; //My Tutor: Lesson Reservations
             $memberEmailData['email'] = $memberInfo->user->email; //recipient (mailto:)
 
