0c6dbc4fa6f1cf3de0bf23c6ae57f4ac66d0eb18
roy <abellana@gmail.com>
2021-07-19 14:22:13 +0800

    throttle to 999

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:500,1',
+            'throttle:999,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
         ],
