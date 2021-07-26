b7dfb6c9749519ba5cc8d21ba91b1f368dc958b1
roy <abellana@gmail.com>
2021-07-27 02:29:50 +0800

    test

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(4343, () => {
+  console.log('listening on *:4343');
 });
