77ea044522457c759209efc87d2dd020f9235472
roy <abellana@gmail.com>
2021-07-27 02:42:12 +0800

    app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('https');
+const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
