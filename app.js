2844dbc7b072fd7c00b44392bf1229a3c82996b2
roy <abellana@gmail.com>
2021-08-06 19:46:30 +0800

    chat server update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
     //res.sendFile(__dirname + '/index.html');
-    res.send('<h1>Hello world</h1>');
+    res.send('<h1>Chat server</h1>');
 });
   
 
