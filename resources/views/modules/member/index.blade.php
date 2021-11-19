63a63a13db0b5663f483f0022b6d1589ee7556ab
Roy <esi.roy.dev@gmail.com>
2021-11-20 04:01:02 +0800

    add member box

--- resources/views/modules/member/index.blade.php
+++ resources/views/modules/member/index.blade.php
@@ -28,6 +28,11 @@
                     <div class="mt-3 mb-4">
                         @include('modules.member.sidebar.reports')
                     </div>
+
+                    <div class="mt-3 mb-4">
+                        @include('modules.member.sidebar.membertestscores')
+                    </div>
+
                 </div>
                 <!--[end sidebar]-->
 
---
+++ resources/views/modules/member/sidebar/membertestscores.blade.php
@@ -0,0 +1,37 @@
+<div class="profile bg-lightred pt-0 px-0">
+    <div class="col-md-12 bg-red text-white pt-2 pb-2 text-center">
+        <strong>テストスコア履歴</strong>
+    </div>
+
+    <div class="col-md-12  pt-2 pb-2 ">
+        <div>
+            <input type="text">
+        </div>
+    </div>
+
+    <div>
+        <div>
+        </div>
+    </div>
+</div>
+
+
+@section('scripts')
+    @parent
+    <script>
+    window.addEventListener('load', function() 
+    {   
+      //alert ("test");
+    });
+    </script>
+@endsection
+
+
+
+@section('styles')
+    @parent
+    <style>
+
+
+    </style>
+@endsection
