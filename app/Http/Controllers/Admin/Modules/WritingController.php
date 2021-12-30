ece61d46667da209ba7bffae5543395fb62c1978
Roy <esi.roy.dev@gmail.com>
2021-12-30 15:08:59 +0800

    fix for auto reply

--- app/Http/Controllers/Admin/Modules/WritingController.php
+++ app/Http/Controllers/Admin/Modules/WritingController.php
@@ -227,7 +227,7 @@ class WritingController extends Controller
 
                     $emailSubject =  $request->subject; //Information on correction service reception
                     $emailMessage =  $formatEntryHTML;
-                    $job = new \App\Jobs\SendAutoReplyJob($emailTo, $emailFrom, $emailSubject, $emailMessage, $emailTemplate, $file);
+                    $job = new \App\Jobs\SendAutoReplyJob($emailTo, $emailFrom, $emailSubject, $emailMessage, $emailTemplate, url($publicURL . basename($uploadFileName)));
                     dispatch($job);  
                 }
             }                    
