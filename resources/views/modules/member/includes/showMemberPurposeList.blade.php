b6a540b83a769b45cbeccb25356edb5613e5f048
Roy <esi.roy.dev@gmail.com>
2022-01-04 20:37:53 +0800

    purpose update

--- resources/views/modules/member/includes/showMemberPurposeList.blade.php
+++ resources/views/modules/member/includes/showMemberPurposeList.blade.php
@@ -1,14 +1,11 @@
 <div>
     @foreach ($purpose as $list)
-
         @php 
             $ctr = 1;
             $options = (array) json_decode($list->purpose_options, true);
             $target_score = (array) json_decode($list->target_scores, true);
         @endphp
-
         <div class="main_list mb-1">
-
             @if (strtolower($list->purpose) == "others")
                 <strong>{{ $list->purpose }}</strong>
                 <div class="option_value_wrapper mb-2 ml-2">
@@ -18,8 +15,6 @@
                 </div>
             @else
                 <strong>{{ $list->purpose }}</strong>
-
-
                 <div class="option_value_wrapper mb-2 ml-2">
                     @foreach ($options as $option_value) 
                         @php 
@@ -29,16 +24,12 @@
                             } else {
                                 $targetScoreText = "";
                             }
-                            
-                            
                         @endphp
                         <span class="option_value">{{$option_value}}{{$targetScoreText}}@if($ctr < count($options)){{","}}@endif</span>
                         @php $ctr++ @endphp
                     @endforeach 
                 </div>
             @endif
-
         </div>
-
     @endforeach
 </div>
