8546f814e121af0ab36def5d36af836ee3d46b68
Roy <esi.roy.dev@gmail.com>
2022-01-27 16:02:27 +0800

    formatting

--- resources/views/emails/writing/mailEntryHTML.blade.php
+++ resources/views/emails/writing/mailEntryHTML.blade.php
@@ -1,55 +1,18 @@
-<div class="container">        
-    @foreach($fieldsArray as $item)
-        
-        <div class='name-wrapper'>
-            <div class="itemName">{{ $item['name'] ?? '' }}</div>        
+<div class="container" style="border:1px solid #e9e9e9">        
+    @foreach($fieldsArray as $item)        
+        <div class='name-wrapper' style="margin:0px 0px 4px; padding: 5px 0px 5px; background-color: #EAF2FA">
+            <div class="itemName" style="margin-left:10px; font-weight:bold; font-size: 14px;">{{ $item['name'] ?? '' }}</div>        
         </div>
-
         @if ($item['type'] == 'uploadfield')
             @if (isset($item['value']))
                 <div class="value-wrapper">
                     <div class="itemValue"><a href="{{ url(Storage::url($item['value'])) }}">{{ basename($item['value']) }}</a></div>
                 </div>
-                @php 
-                    //$writingFields = new \App\Models\WritingEntries;
-                    //$writingFields->generateFileAnchorLink( $item['value'] );
-                @endphp
             @endif
         @else
             <div class="value-wrapper">
-                <div class="itemValue">{!! $item['value'] ?? '' !!}</div>
+                <div class="itemValue" style="margin-left: 25px; min-height:15px; padding-bottom: 3px">{!! $item['value'] ?? '' !!}</div>
             </div>
         @endif
-
     @endforeach
 </div>
-
-<style>
-    div {
-        font-size: 13px;
-    }
-
-    .container {
-        border:1px solid #e9e9e9
-    }
-
-    .name-wrapper {
-        margin:0px 0px 4px;
-        padding: 5px 0px 5px;
-        background-color: #EAF2FA
-    }    
-
-    .itemName {
-        margin-left:10px; 
-        font-weight:bold;
-        font-size: 14px;
-    }
-
-    .itemValue {
-        margin-left: 25px;
-        min-height:15px;
-        padding-bottom: 3px
-    }
-
-
-</style>
