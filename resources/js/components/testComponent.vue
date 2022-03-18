fa37e045e7b80dc442db56458854421e1293ec01
Roy <esi.roy.dev@gmail.com>
2022-03-18 19:48:22 +0800

    test

--- resources/js/components/testComponent.vue
+++ resources/js/components/testComponent.vue
@@ -3,7 +3,9 @@ export default {
   name: "testComponent",
   data() {
     return {
-      message: 'this is the test of the words that gets upper case every thing!'
+      message: 'this is the test of the words that gets upper case every thing!',
+      u_message: 'this_is_the_test_of_the_words_that_gets_upper_case_every thing!',
+      objectKeys: "ThisIsAKeyThatIsMergedTogetherAndNeedsToBeSplitted"
     }
   },
   methods: {
@@ -14,8 +16,22 @@ export default {
       }
       let words = wordArray.join(" ");
       return words;
-    }
-    
+    },
+    removeUnderscore(string) {
+      let wordArray = string.split("_");
+      let words = wordArray.join(" ");
+      return words;
+    },    
+    capitalizeFirstLetter(string) {
+        let words = this.removeUnderscore(string);
+        let newString = words.charAt(0).toUpperCase() + words.slice(1); 
+        return newString.trim(); 
+    },   
+    FormatObjectKey(string) {
+      let wordArray = string.split(/(?=[A-Z])/);
+      let words = wordArray.join(" ");
+      return words;
+    },
   }
 }
 </script>
@@ -23,6 +39,8 @@ export default {
 <template>
   <div>
     <h1>1. {{ ucwords(message)  }}</h1>
+    <h1>2. {{ capitalizeFirstLetter(u_message)  }}</h1>
+    <h1>2. {{ FormatObjectKey(objectKeys)  }}</h1>
    
   </div>
 </template>
