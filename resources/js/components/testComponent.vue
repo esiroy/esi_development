70d612999630f38305a63beea31310469fd8397a
Roy <esi.roy.dev@gmail.com>
2022-03-18 18:38:31 +0800

    public layout

--- resources/js/components/testComponent.vue
+++ resources/js/components/testComponent.vue
@@ -9,11 +9,27 @@ export default {
   methods: {
     ucwords(string) {
         return string.toLowerCase().replace(/(?<= )[^\s]|^./g, a=>a.toUpperCase())  
-    }  
+    },
+    FormatObjectKey(string) {
+        let newString = string.charAt(0).toUpperCase() + string.slice(1);
+        newString = newString.replace(/_/g, " ")
+
+        //add space before big letters
+        return newString.replace(/([A-Z])/g, ' $1').trim(); 
+    },
+    capitalizeFirstLetter(string) {
+        let newString = string.charAt(0).toUpperCase() + string.slice(1);    
+        newString = newString.replace(/_/g, " ")       
+        return newString.trim(); 
+    },    
   }
 }
 </script>
 
 <template>
-  <h1>{{ ucwords(message)  }}</h1>
+  <div>
+    <h1>1. {{ ucwords(message)  }}</h1>
+    <h1>2. {{ FormatObjectKey(message)  }}</h1>
+    <h1>3. {{ FormatObjectKey(message)  }}</h1>
+  </div>
 </template>
