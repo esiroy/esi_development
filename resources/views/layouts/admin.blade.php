75fb0cf5a65d23d0cd2c502b00dee4b2b42de3f4
Roy <esi.roy.dev@gmail.com>
2022-07-12 20:06:39 +0800

    update pagination

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,10 +18,10 @@
     <link rel="preconnect" href="//fonts.gstatic.com" crossorigin />
     <link rel="preconnect" href="//fonts.googleapis.com" crossorigin />
     <link rel="preconnect" href="//cdn.datatables.net" rel="preconnect" crossorigin />
-    <link rel="preload" href="{{ asset('css/app.css') .'?id=version_5_3' }}" as="style">
+    <link rel="preload" href="{{ asset('css/app.css') .'?id=version_5_4' }}" as="style">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css')  .'?id=version_5_3' }}">
-    <script src="{{ asset('js/admin.js') .'?id=version_5_3'  }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css')  .'?id=version_5_4' }}">
+    <script src="{{ asset('js/admin.js') .'?id=version_5_4'  }}" defer></script>
     
     <noscript>
         <link rel="stylesheet" type="text/css" href="">
