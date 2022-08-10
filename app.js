6f747ea27fcc1d2f04bd3c46038d6a9bb589d2b5
Roy <esi.roy.dev@gmail.com>
2022-08-10 16:03:00 +0800

    canvas server

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var serverPort = 30002;
+var serverPort = 50001;
 
 
 const express = require('express');
 const app = express();
 
 const server = app.listen(serverPort, function() {
-    console.log('server running on port 3002');
+    console.log('server running on port ' + serverPort);
 });
 
 const io = require('socket.io')(server);
@@ -22,36 +22,53 @@ var users = [];
 
 
 io.on('connection', function(socket) {
-    //console.log("user connected, with id " + socket.id)
+
+    console.log("user connected, with id " + socket.id)
+
+
+    socket.on("SEND_DRAWING", (canvasData) => {
+        io.to('' + canvasData.channelid + '').emit('UPDATE_DRAWING', canvasData);
+    });
+
+
+
+    socket.on("START_SLIDER", (data) => {
+        io.sockets.emit("START_SLIDER", data);
+    });
+
+
+
+    socket.on("CALL_USER", (data) => {
+
+        io.sockets.emit("CALL_USER", data);
+
+    });
+
+
+    socket.on("DROP_CALL", (data) => {
+        io.sockets.emit("DROP_CALL", data);
+    });
+
+
+
+    socket.on("ACCEPT_CALL", (data) => {
+        io.sockets.emit("ACCEPT_CALL", data);
+    });
+
+
 
     socket.on("SEND_USER_MESSAGE", function(data) {
-
-        console.log("send user ", data.recipient.username);
-
-        //io.sockets.connected[data.id].emit("PRIVATE_MESSAGE", data);
-        /*
-        for (var i in users) {
-
-            if (data.recipient.username == users[i].username) 
-            {              
-                io.sockets.connected[users[i].id].emit("PRIVATE_MESSAGE", data);
-                //break;
-            }            
-        }*/
-
         io.sockets.emit("PRIVATE_MESSAGE", data);
-
-        //this.handleUserPrivateMsg(data);
     });
 
     socket.on("SEND_OWNER_MESSAGE", function(data) {
-
         io.emit('OWNER_MESSAGE', data);
         //this.handleUserPrivateMsg(data);
     });
 
     /*Register connected user*/
     socket.on('REGISTER', function(user) {
+
         console.log("user connected, with id " + socket.id + " " + user.username)
 
         //remove if ever there is same userid
@@ -63,16 +80,22 @@ io.on('connection', function(socket) {
             }
         }*/
 
+        socket.join(user.channelid);
+
+
+
         users = users.filter(function(element) {
             return element !== undefined;
         });
 
         users.push({
             'id': socket.id,
+            'channelid': user.channelid,
             'userid': user.userid,
             'username': user.username,
             'user_image': user.user_image,
             'nickname': user.nickname,
+            'status': user.status,
             'type': user.type,
         });
 
