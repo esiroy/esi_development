b4dfa4eb15ffbde16a2b583fc0eb9ff2b24af698
Roy <esi.roy.dev@gmail.com>
2022-09-14 21:28:10 +0800

    update try cacthc writing

--- app/Http/Controllers/Writing/WritingController.php
+++ app/Http/Controllers/Writing/WritingController.php
@@ -26,42 +26,49 @@ class WritingController extends Controller
     
     public function index(FormFields $formFieldModel) 
     {
-        $member = Member::where('user_id', Auth::user()->id)->first();
-        if (isset($member)) {
+        try {
 
-            /* FRONT END WRITING FORM */
+            $member = Member::where('user_id', Auth::user()->id)->first();
+            if (isset($member)) {
 
-            $form_id = 1; //all forms are 1(for now)
-            $formFields = FormFields::where('form_id', $form_id)->where('page_id', 0)->orderBy('sequence_number', 'ASC')->get();
-            $formFieldHTML[] = "";
+                /* FRONT END WRITING FORM */
 
-            $cfields = $formFields;        
-            foreach ($formFields as $formField) 
-            {
-                $formFieldHTML[] = $formFieldModel->generateFrontEndFormFieldHTML($formField, $cfields);             
-            }
+                $form_id = 1; //all forms are 1(for now)
+                $formFields = FormFields::where('form_id', $form_id)->where('page_id', 0)->orderBy('sequence_number', 'ASC')->get();
+                $formFieldHTML[] = "";
 
-            /************ GET CHILDREN HTML ************/
-            $formFieldChildrenHTML[] = "";
-            //Get Pages
-            $pages =  FormFields::distinct()->where('page_id', '>=', 1)->orderBy('page_id', 'ASC')->get(['page_id']);
-            $pageCounter =  $pages->count() + 1;
-
-            foreach ($pages as $page) 
-            {           
-                $formChildFields = FormFields::where('form_id', $form_id)->where('page_id', $page->page_id)->orderBy('sequence_number', 'ASC')->get();
-                $child_cfields = FormFields::where('form_id', $form_id)->orderBy('sequence_number', 'ASC')->get();
-                foreach ($formChildFields as $formChildField) 
+                $cfields = $formFields;        
+                foreach ($formFields as $formField) 
                 {
-                    $formFieldChildrenHTML[$page->page_id][] =  $formFieldModel->generateFrontEndFormFieldHTML($formChildField, $child_cfields);
+                    $formFieldHTML[] = $formFieldModel->generateFrontEndFormFieldHTML($formField, $cfields);             
                 }
+
+                /************ GET CHILDREN HTML ************/
+                $formFieldChildrenHTML[] = "";
+                //Get Pages
+                $pages =  FormFields::distinct()->where('page_id', '>=', 1)->orderBy('page_id', 'ASC')->get(['page_id']);
+                $pageCounter =  $pages->count() + 1;
+
+                foreach ($pages as $page) 
+                {           
+                    $formChildFields = FormFields::where('form_id', $form_id)->where('page_id', $page->page_id)->orderBy('sequence_number', 'ASC')->get();
+                    $child_cfields = FormFields::where('form_id', $form_id)->orderBy('sequence_number', 'ASC')->get();
+                    foreach ($formChildFields as $formChildField) 
+                    {
+                        $formFieldChildrenHTML[$page->page_id][] =  $formFieldModel->generateFrontEndFormFieldHTML($formChildField, $child_cfields);
+                    }
+                }
+
+                return view("modules.writing.index", compact('pages', 'pageCounter', 'form_id','formFields', 'formFieldHTML', 'formFieldChildrenHTML'));
+
+            } else {
+                sleep(3);
+                return redirect()->away( url('/admin/writing'));        
             }
+           
+        }  catch(\Exception $e) {
 
-            return view("modules.writing.index", compact('pages', 'pageCounter', 'form_id','formFields', 'formFieldHTML', 'formFieldChildrenHTML'));
-
-        } else {
-            sleep(3);
-            return redirect()->away( url('/admin/writing'));        
+             echo $e->getMessage();
         }
     }
 
