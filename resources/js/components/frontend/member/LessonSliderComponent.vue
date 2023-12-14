e874c322b24adc7e5e3d3e759a7c1c726c3bfb29
esiroy <abellana@gmail.com>
2023-12-14 22:50:37 +0800

    update toast wdith

--- resources/js/components/frontend/member/LessonSliderComponent.vue
+++ resources/js/components/frontend/member/LessonSliderComponent.vue
@@ -1744,7 +1744,7 @@ export default {
 
 <style lang="scss" >
     .custom-toast-width {
-        width: 190px !important; 
+        width: 170px !important; 
         font-size: 12px;
     }
 </style>
