5ebaee4e9f2f9d1c9be0fb502867a6631471c7fb
Roy Abellana <abellana@gmail.com>
2024-05-02 10:44:31 +0800

    update

--- app/Models/ReportCard.php
+++ app/Models/ReportCard.php
@@ -27,6 +27,38 @@ class ReportCard extends Model
         return $latestReportCard;
     }
 
+
+    public function getAllLatestByMultiID($memberID, $accountID, $perPage = 10) {
+
+        if ($accountID == null || $accountID == 1) { //merge result from account 1 and old account which is (null)
+            //No multi account selected
+            $reportCards = ReportCard::select('report_card.*', 'schedule_item.lesson_time', 'schedule_item.member_multi_account_id')
+            ->join('schedule_item', 'report_card.schedule_item_id', '=', 'schedule_item.id')
+            ->where('report_card.member_id', $memberID)
+            ->where('report_card.valid', true)
+            ->where(function ($query) use ($accountID) {
+                $query->where('schedule_item.member_multi_account_id', 1)
+                      ->orWhere('schedule_item.member_multi_account_id', null); // Main account
+            })
+            ->orderBy('schedule_item.lesson_time', 'DESC')
+            ->paginate($perPage, ['*'], 'reportcards');           
+
+        } else {
+            
+            echo "Test";
+            
+            $reportCards = ReportCard::select('report_card.*', 'schedule_item.lesson_time', 'schedule_item.member_multi_account_id')
+            ->join('schedule_item', 'report_card.schedule_item_id', '=', 'schedule_item.id')
+            ->where('report_card.member_id', $memberID)
+            ->where('schedule_item.member_multi_account_id', $accountID)
+            ->where('report_card.valid', true)
+            ->orderBy('schedule_item.lesson_time', 'DESC')            
+            ->paginate($perPage, ['*'], 'reportcards');            
+        }        
+
+        return $reportCards;
+    }
+
     public function getLatestbyMultiID($memberID, $accountID) {
 
         $latestReportCard = ReportCard::select('report_card.*', 'schedule_item.lesson_time', 'schedule_item.member_multi_account_id')
